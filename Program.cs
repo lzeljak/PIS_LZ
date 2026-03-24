64c7787d7ae2a467b8a4cf61e5f50e4ffa4488cc
lzeljak <lovrezeljak123@gmail.com>
2026-03-24 19:01:45 +0100

    dodan "Novoo"

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System;
+
 Console.WriteLine("Hello, World!");
+
+
+static int Main()
+{
+    Console.WriteLine("Novoo");
+    return 0;
+}
